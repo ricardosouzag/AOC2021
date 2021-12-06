721db63a476dd0c2d7c93cdc6f4de0d8b4bc4d79
ricardosouzag <gradow@gmail.com>
2021-12-06 03:24:51 -0300

    All it took was thinking like a feesh.

---
+++ day6.py
@@ -0,0 +1,27 @@
+def Day(fishes):
+    aux = list(range(9))
+    for i in range(1,9):
+        aux[i-1] = fishes[i]
+    aux[6] += fishes[0]
+    aux[8] = fishes[0]
+    return aux
+
+with open("day6.txt") as f:
+    origfish = f.read().split(',')
+    origfish = [int(fish) for fish in origfish]
+    fishbuckets = [origfish.count(i) for i in range(9)]
+    print(fishbuckets)
+
+    feesh = fishbuckets
+
+    for i in range(80):
+        feesh = Day(feesh)
+
+    print('Parte 1:',sum(feesh))
+
+    feesh = fishbuckets
+
+    for i in range(256):
+        feesh = Day(feesh)
+
+    print('Parte 2:',sum(feesh))
