99451bb47c4883803b6aed2e708cc89cf4294406
ricardosouzag <gradow@gmail.com>
2021-12-06 12:34:40 -0300

    Improved day1 and added day2 in Haskell

--- day1.hs
+++ day1.hs
@@ -8,12 +8,12 @@ main = do
         print . countIncreases2 input $ 0
 
 countIncreases :: [Int] -> Int -> Int                      
-countIncreases [r] n    = n
+countIncreases [r]    n = n
 countIncreases (r:rs) n | head rs > r = countIncreases rs (n+1)
                         | otherwise = countIncreases rs n
 
 
 countIncreases2 :: [Int] -> Int -> Int                      
-countIncreases2 [r1,r2,r3] n = n
+countIncreases2 [r1,r2,r3]    n = n
 countIncreases2 (r1:r2:r3:rs) n | head rs > r1 = countIncreases2 (r2:r3:rs) (n+1)
                                 | otherwise = countIncreases2 (r2:r3:rs) n
