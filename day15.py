6b78ef5598c78971cff5f751c498f55eaa6512db
ayugradow <gradow@gmail.com>
2021-12-16 00:14:48 -0300

    Finally done with day15. TIL how to implement A*.

--- day15.py
+++ day15.py
@@ -1,35 +1,37 @@
-import copy
 from itertools import product
-
+from queue import PriorityQueue
 import numpy as np
 
 
-def GetNeighbors(coord):
+def GetNeighbors(coord, grid):
 	x, y = coord
-	neighbors = {(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)} & coords
-	if end in neighbors:
-		neighbors = {end}
+	neighbors = {(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)} & grid
 	return neighbors
 
 
-def FindMaxPathLength(org):
-	stack = [(org, set(), 0)]
-	visited = {}
-	minpathlen = 9 * (r + c)
-	while stack:
-		present, past, value = stack.pop()
-		if present in visited:
-			if visited[present] < value:
-				continue
-			else:
-				visited[present] = value
-		else:
-			visited[present] = value
-		past |= {present}
-		if present == end:
-			minpathlen = min(minpathlen, value + map[present])
-		stack += [(neigh, past.copy(), value + map[present]) for neigh in GetNeighbors(present) - past]
-	return minpathlen
+def H(point, target): return abs(point[0] - target[0]) + abs(point[1] - target[1])
+
+
+def MinimizeCost(source, target, grid):
+	rows, cols = grid.shape
+	points = set(product(range(rows), range(cols)))
+
+	opens = PriorityQueue()
+	openset = {source:0}
+	opens.put((H(source, target) + openset[source], source))
+
+	while opens:
+		h, node = opens.get()
+		g = openset[node]
+
+		for neigh in GetNeighbors(node, points):
+			if neigh == target:
+				return g + grid[target]
+			neigh_g = g + grid[neigh]
+			neigh_h = H(neigh, target)
+			if neigh not in openset or openset[neigh] > neigh_g:
+				openset.update({neigh:neigh_g})
+				opens.put((neigh_h + neigh_g, neigh))
 
 
 with open('day15.txt') as f:
@@ -41,4 +43,15 @@ with open('day15.txt') as f:
 	start = (0, 0)
 	end = (r - 1, c - 1)
 
-	print(FindMaxPathLength(start) - 1)
+	print('Parte 1:', MinimizeCost(start, end, map))
+
+	map2 = np.zeros((5 * r, 5 * c), dtype=int)
+	for i in range(5):
+		for j in range(5):
+			for x, y in coords:
+				map2[i * r + x, j * c + y] = ((map[x, y] + i + j - 1) % 9) + 1
+
+	r2, c2 = map2.shape
+	start2, end2 = (0, 0), (r2 - 1, c2 - 1)
+
+	print('Parte 2:', MinimizeCost(start2, end2, map2))
