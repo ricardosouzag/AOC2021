fbce5266ce136e17a55d7cc59bedb98cb5218c87
ricardosouzag <gradow@gmail.com>
2021-12-17 14:05:59 -0300

    Day 15, part2 should work but doesn't. RIP = WIP

--- day16.py
+++ day16.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def binToInt(bin):
 	return int(''.join(bin), 2)
 
@@ -59,9 +62,9 @@ class Packet:
 				cont = self._encodedBits[5 * cnt]
 				getliteral.append(self._encodedBits[cnt * 5 + 1:cnt * 5 + 6])
 				cnt += 1
-			self.literal = int(''.join(getliteral), 2)
+			self.value = int(''.join(getliteral), 2)
 			self._currLen = 6 + cnt * 5
-			print(self.bits[:self._currLen])
+			# print(self.bits[:self._currLen])
 			if not self._ignorePadding:
 				self._padding = 4 - (self._currLen % 4)
 		else:
@@ -69,7 +72,7 @@ class Packet:
 			self._LTIInstLen = self._LTIdict[self.LTI]
 			self.LTIInst = binToInt(self._encodedBits[1:self._LTIInstLen + 1])
 			self._currLen = 6 + 1 + self._LTIInstLen
-			print(self.bits[:self._currLen])
+			# print(self.bits[:self._currLen])
 			if self.LTI == 0:
 				self._padding = self.LTIInst
 				self.subpackets = GetPackets(
@@ -78,7 +81,7 @@ class Packet:
 						)[0]
 			else:
 				self.subpackets, leftover = GetPackets(self._encodedBits[self._LTIInstLen + 1:], self.LTIInst, True)
-				self._padding = sum([packet.size for packet in self.subpackets]) + len(leftover)
+				self._padding = sum([packet.size for packet in self.subpackets])
 
 	def GetVersions(self):
 		if any(self.subpackets):
@@ -86,9 +89,31 @@ class Packet:
 		return self.version
 
 
+def ParsePacket(packet):
+	if packet.type == 4:
+		return packet.value
+	subpackets = packet.subpackets
+	subvalues = [ParsePacket(subpack) for subpack in subpackets]
+	if packet.type == 0:
+		return sum(subvalues)
+	if packet.type == 1:
+		return np.prod(subvalues)
+	if packet.type == 2:
+		return min(subvalues)
+	if packet.type == 3:
+		return max(subvalues)
+	if packet.type == 5:
+		return 1 if subvalues[0] > subvalues[1] else 0
+	if packet.type == 6:
+		return 0 if subvalues[0] > subvalues[1] else 1
+	if packet.type == 7:
+		return 1 if subvalues[0] == subvalues[1] else 0
+
+
 with open('day16.txt') as f:
 	raw = f.read().splitlines()
 	raw = [list(e) for e in raw][0]
 	inputcode = ''.join([hex2bin[c] for c in raw])
-	print(inputcode)
-	print(sum([pack.GetVersions() for pack in GetPackets(inputcode)[0]]))
+	packets = GetPackets(inputcode)[0]
+	print('Parte 1:', sum([pack.GetVersions() for pack in packets]))
+	print('Parte 2:', ParsePacket(packets[0]))
